a2d937aa12c80ec6680627148f46a0f75f062483
Aleksey <aleksei.travnov@mail.ru>
2022-12-12 00:30:49 +0300

    complete 3 steps

---
+++ Makefile
@@ -0,0 +1,17 @@
+install:
+		npm ci
+
+lint:
+		npx eslint .
+
+publish:
+		npm publish --dry-run
+
+gendiff:
+		node bin/gendiff.js
+
+test:
+		npm test
+
+test-coverage:
+		npm test -- --coverage --coverageProvider=v8
---
+++ src/parse.js
@@ -0,0 +1,11 @@
+
+const parse = (file, format) => {
+    switch (format) {
+        case 'json':
+            return JSON.parse(file);
+        default:
+            throw new Error (`${format} is wrong format file.`);
+    }
+};
+
+export default parse
