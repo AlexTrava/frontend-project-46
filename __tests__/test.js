47dc443c3bb539a2afca14358255ee8b3b034e51
Aleksey <aleksei.travnov@mail.ru>
2023-01-05 01:16:34 +0300

    add formatters, add test

--- __tests__/test.js
+++ __tests__/test.js
@@ -4,22 +4,21 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import genDiff from '../src/index.js';
 import expectedJson from '../__fixtures__/expectedJson.js';
-import expectedStylish from '../__fixtures__/expectedStylish.js'
+import expectedStylish from '../__fixtures__/expectedStylish.js';
 import expectedPlain from '../__fixtures__/expectedPlain.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-
 describe('genDiff App', () => {
-  test('test json format', () => {
+  test('json format', () => {
     expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expectedStylish);
     expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toEqual(expectedStylish);
     expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(expectedPlain);
     expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(expectedJson);
   });
-  test('test yaml format', () => {
+  test('yaml format', () => {
     expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toEqual(expectedStylish);
     expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'stylish')).toEqual(expectedStylish);
     expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain')).toEqual(expectedPlain);
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,8 +6,6 @@ const getChildren = (node) => node.children;
 const getType = (node) => node.type;
 const spaceCount = 2;
 
-
-
 const stringify = (file, depth = 1) => {
   const indentSize = depth * spaceCount;
   const currentIndent = ' '.repeat(indentSize + 2);
@@ -17,8 +15,8 @@ const stringify = (file, depth = 1) => {
     return `${file}`;
   }
   const output = Object.entries(file)
-                       .map(([key, value]) => `${currentIndent}${key}: ${stringify(value, depth + 2)}`);
-  return ['{',...output,`${bracketIndent}}`].join('\n');
+    .map(([key, value]) => `${currentIndent}${key}: ${stringify(value, depth + 2)}`);
+  return ['{', ...output, `${bracketIndent}}`].join('\n');
 };
 
 const stylish = (tree, depth = 1) => {
@@ -26,35 +24,34 @@ const stylish = (tree, depth = 1) => {
   const currentIndent = ' '.repeat(indentSize);
   const bracketIndent = ' '.repeat(indentSize - spaceCount);
 
-    const lines = tree.flatMap((node) => {
-      const type = getType(node);
-      const key = getKey(node);
-      const value = getValue(node);
-      const children = getChildren(node);
-      switch (type) {
-        case 'nested': {
-          return `${' '.repeat(indentSize + 1)} ${key}: ${stylish(children, depth + 2)}`;
-        }
-        case 'added': {
-          return `${currentIndent}+ ${key}: ${stringify(value, depth + 2)}`;
-        }
-        case 'removed': {
-          return `${currentIndent}- ${key}: ${stringify(value, depth + 2)}`;
-        }
-        case 'unchanged': {
-          return `${currentIndent}  ${key}: ${stringify(value, depth + 2)}`;
-        }
-        case 'changed': {
-          const beforeChanged = `${currentIndent}- ${key}: ${stringify(node.value1, depth + 2)}`;
-          const afterChanged = `${currentIndent}+ ${key}: ${stringify(node.value2, depth + 2)}`;
-          return [beforeChanged, afterChanged];
-        }
-        default:
-          throw new Error(`Unknown type - ${type}.`);
+  const lines = tree.flatMap((node) => {
+    const type = getType(node);
+    const key = getKey(node);
+    const value = getValue(node);
+    const children = getChildren(node);
+    switch (type) {
+      case 'nested': {
+        return `${' '.repeat(indentSize + 1)} ${key}: ${stylish(children, depth + 2)}`;
       }
-    });
-    return ['{', ...lines, `${bracketIndent}}`].join('\n');
-
+      case 'added': {
+        return `${currentIndent}+ ${key}: ${stringify(value, depth + 2)}`;
+      }
+      case 'removed': {
+        return `${currentIndent}- ${key}: ${stringify(value, depth + 2)}`;
+      }
+      case 'unchanged': {
+        return `${currentIndent}  ${key}: ${stringify(value, depth + 2)}`;
+      }
+      case 'changed': {
+        const beforeChanged = `${currentIndent}- ${key}: ${stringify(node.value1, depth + 2)}`;
+        const afterChanged = `${currentIndent}+ ${key}: ${stringify(node.value2, depth + 2)}`;
+        return [beforeChanged, afterChanged];
+      }
+      default:
+        throw new Error(`Unknown type - ${type}.`);
+    }
+  });
+  return ['{', ...lines, `${bracketIndent}}`].join('\n');
 };
 
 export default stylish;
